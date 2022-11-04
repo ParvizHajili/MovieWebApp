5343180ac6306d4761ffce9c879f5fbd168557ab
Parviz <parviz.hajili@gmail.com>
2022-11-04 15:43:05 +0400

    Serverside and clientside validation

--- MovieWebApp/Models/Movie.cs
+++ MovieWebApp/Models/Movie.cs
@@ -5,11 +5,13 @@ namespace MovieWebApp.Models
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(50, MinimumLength = 5)]
         public string Title { get; set; } = string.Empty;
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
+        [Range(0,100)]
         public decimal Price { get; set; }
     }
 }
